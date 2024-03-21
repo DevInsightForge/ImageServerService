18122efece21efee5238204d18f7dcdfa71ae102
Mofajjal Rasul <mofajjal.rasul@technonext.com>
2024-03-21 11:18:29 +0600

    service: add cache support

--- ImageServer/Program.cs
+++ ImageServer/Program.cs
@@ -3,22 +3,29 @@ using ImageServer.Services;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateSlimBuilder(args);
+builder.Services.AddResponseCaching();
 builder.Services.ConfigureHttpJsonOptions(options =>
 {
     options.SerializerOptions.TypeInfoResolverChain.Insert(0, CustomJsonSerializerContext.Default);
 });
+builder.Services.AddOutputCache();
+builder.Services.AddResponseCaching();
 
 builder.Services.AddHttpClient<ImageResizeService>();
 builder.Services.AddScoped<ImageResizeService>();
+
 var app = builder.Build();
+app.UseOutputCache();
+app.UseResponseCaching();
+
 app.MapGet("/resize", async (
     [FromQuery(Name = "u")] string? imageUrl,
     [FromQuery(Name = "w")] int? width,
-    [FromQuery(Name = "w")] int? height,
-    [FromQuery(Name = "w")] int? quality,
+    [FromQuery(Name = "h")] int? height,
+    [FromQuery(Name = "q")] int? quality,
     ImageResizeService resizeService) =>
 {
     return await resizeService.ResizeImageAsync(imageUrl, width, height, quality);
-});
+}).CacheOutput();
 
 app.Run();
