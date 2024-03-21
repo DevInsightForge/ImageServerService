83dfa142e4a1512492bde88e7716b5523eb0ae1d
Mofajjal Rasul <imzihad@gmail.com>
2024-03-21 12:50:37 +0600

    server: only use output cache and remove response cache

--- ImageServer/Program.cs
+++ ImageServer/Program.cs
@@ -1,35 +1,26 @@
 using ImageServer.Contexts;
 using ImageServer.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Net.Http.Headers;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddResponseCaching();
 builder.Services.AddOutputCache(options =>
-    options.DefaultExpirationTimeSpan = TimeSpan.FromDays(1));
-builder.Services.ConfigureHttpJsonOptions(options => 
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, CustomJsonSerializerContext.Default));
+{
+    options.UseCaseSensitivePaths = false;
+    options.DefaultExpirationTimeSpan = TimeSpan.FromDays(1);
+    options.AddBasePolicy(policy => policy.SetVaryByQuery("*"));
+});
+builder.Services.ConfigureHttpJsonOptions(options =>
+{
+    options.SerializerOptions.TypeInfoResolverChain.Insert(0, CustomJsonSerializerContext.Default);
+});
 
 builder.Services.AddHttpClient<ImageResizeService>();
 builder.Services.AddScoped<ImageResizeService>();
 
 var app = builder.Build();
-
 app.UseOutputCache();
-app.UseResponseCaching();
 
-app.Use(async (context, next) =>
-{
-    context.Response.GetTypedHeaders().CacheControl = new CacheControlHeaderValue()
-    {
-        Public = true,
-        MaxAge = TimeSpan.FromDays(1),
-    };
-    await next();
-});
-
-app.MapGet("/resize", async (
+app.MapGet("/resize",async (
     [FromQuery(Name = "u")] string? imageUrl,
     [FromQuery(Name = "w")] int? width,
     [FromQuery(Name = "h")] int? height,
@@ -37,6 +28,6 @@ app.MapGet("/resize", async (
     ImageResizeService resizeService) =>
 {
     return await resizeService.ResizeImageAsync(imageUrl, width, height, quality);
-}).CacheOutput();
+}).CacheOutput(options => options.SetVaryByQuery("*"));
 
 app.Run();
